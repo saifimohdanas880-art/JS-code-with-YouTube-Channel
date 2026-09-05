0606c9725abed4d23ce72fa165a594387895f429
saifimohdanas880-art <saifimohdanas880@gmail.com>
2026-09-05 09:12:52 +0000

    datatypes

---
+++ 01_basics/data.js
@@ -0,0 +1,28 @@
+"use strict";//treat all js code as newer version of js
+//alert("hello world"  /  3 + 3 )// we are using node.js not browser so alert will not work
+
+console.log(3 + 3) //code readability should be high
+
+/*  DATA TYPES
+   // 1. PRIMITIVE DATA TYPES
+   // 
+   
+   //2. NON-PRIMITIVE DATA TYPES
+//3. UNDEFINED DATA TYPES- value is not assigned to variable / value not defined
+
+//4. NULL DATA TYPES-stanalone value
+//5. NUMBER DATA TYPES- 2 to power 53 
+//6.symbol - used for unique identifiers
+//7.bigint -  used treding and stock market or make large number codes
+//.8string - used for ""/ ''/``
+//9.boolean - true/false
+//10. object - used for storing multiple values in key value pair //ex ---- console.log({name:"sachin", age: 30,) 
+sachine -- string 
+30 -- number
+
+null -- object
+
+
+
+
+   */
